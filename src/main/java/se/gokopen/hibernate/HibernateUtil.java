f05c86f2005ff9689aa041ecdc2a63ceaef310b0
Anders Pikas <anders@pikas.se>
2015-03-08 14:49:25 +0100

    Fixed deprecated warning for Hibernate.

--- src/main/java/se/gokopen/hibernate/HibernateUtil.java
+++ src/main/java/se/gokopen/hibernate/HibernateUtil.java
@@ -1,14 +1,11 @@
 package se.gokopen.hibernate;
 
-
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 import se.gokopen.model.PatrolImpl;
 import se.gokopen.model.ScoreImpl;
@@ -22,18 +19,22 @@ public class HibernateUtil {
 //	private static Logger logger = Logger.getLogger("se.goteborg.livsmedelslokal.hibernate");
 	
 	private static final SessionFactory sessionFactory;
-	
+	private static ServiceRegistry serviceRegistry;
+
 	    static {
 	        try {
 	        	System.out.println("Drar igång");
-//	        	logger.log(Level.CONFIG,"Create Sessionfactory, load classes.");	
-	            sessionFactory = new Configuration()	    
-	            	.addAnnotatedClass(PatrolImpl.class)
+//	        	logger.log(Level.CONFIG,"Create Sessionfactory, load classes.");
+	        	Configuration configuration = new Configuration().addAnnotatedClass(PatrolImpl.class)
 	            	.addAnnotatedClass(ScoreImpl.class)
 	            	.addAnnotatedClass(Station.class)
 	            	.addAnnotatedClass(Track.class)
-	            	.configure("/se/gokopen/hibernate/mysql.cfg.xml").buildSessionFactory();   //lokal utv
-	
+	            	.configure("/se/gokopen/hibernate/mysql.cfg.xml");   //lokal utv
+	            serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+	                configuration.getProperties()).build();
+	            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	            
+	            
 	        } catch (Throwable ex) {
 	            // exception!
 	        	ex.printStackTrace();
