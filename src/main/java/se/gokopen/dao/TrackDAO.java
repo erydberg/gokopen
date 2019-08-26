9b403d4c800940faa47b43104a60581c083e26d1
Erik Rydberg <erik.rydberg@intraservice.goteborg.se>
2019-08-26 16:40:44 +0200

    Anpassning till dockerpaketering. Bryter vanlig deploy-möjlighet då sökväg till databasserver blir annorlunda.

--- src/main/java/se/gokopen/dao/TrackDAO.java
+++ src/main/java/se/gokopen/dao/TrackDAO.java
@@ -10,38 +10,42 @@ import se.gokopen.model.Track;
 
 @Repository
 public class TrackDAO {
-	
-	@Autowired
+
+    @Autowired
     private SessionFactory sessionFactory;
-	
-	public void save(Track track) throws TrackNotSavedException{
-		sessionFactory.getCurrentSession().saveOrUpdate(track);
-	}
-	
-	public void delete(Track track) throws TrackNotFoundException{
-		sessionFactory.getCurrentSession().delete(track);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public Track getById(Integer id) throws TrackNotFoundException{
-		Track track = null;
-		
-		List<Track> tracks = (List<Track>) sessionFactory.getCurrentSession().createQuery("from Track track where track.trackId=?").setParameter(0, id).list();
-		if (tracks==null || tracks.isEmpty() || tracks.size()>1){
-			throw new TrackNotFoundException("Hittar inte spåret med id: " + id);
-		}
-		track = tracks.get(0);
-		return track;
-	}
-	
-	public void deleteId(Integer id) throws TrackNotFoundException{
-		Track track = getById(id);
-		delete(track);
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<Track> getAllTracks(){
-		List<Track> tracks = sessionFactory.getCurrentSession().createQuery("from Track").list();
-		return tracks;
-	}
+
+    public void save(Track track) {
+        sessionFactory.getCurrentSession().saveOrUpdate(track);
+    }
+
+    public void delete(Track track) {
+        sessionFactory.getCurrentSession().delete(track);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Track getById(Integer id) throws TrackNotFoundException {
+        Track track = null;
+
+        List<Track> tracks = (List<Track>) sessionFactory.getCurrentSession()
+                .createQuery("from Track track where track.trackId= :trackid").setParameter("trackid", id).list();
+        if (tracks == null || tracks.isEmpty() || tracks.size() > 1) {
+            throw new TrackNotFoundException("Hittar inte spåret med id: " + id);
+        }
+        track = tracks.get(0);
+        return track;
+    }
+
+    public void deleteId(Integer id) {
+        try {
+            Track track = getById(id);
+            delete(track);
+        } catch (TrackNotFoundException e) {
+            System.out.println("Spåret fanns inte att ta bort");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Track> getAllTracks() {
+        return sessionFactory.getCurrentSession().createQuery("from Track").list();
+    }
 }
